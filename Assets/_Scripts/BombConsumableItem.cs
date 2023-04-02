9dd416153f650ac06d7a1fc4ae36aeaf8d4fa484
Jesper Lundquist <96360325+jeppaleno@users.noreply.github.com>
2023-04-02 21:42:31 +0200

    Firebomb foundation

---
+++ Assets/_Scripts/BombConsumableItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Consumeables/Bomb Item")]
+public class BombConsumableItem : ConsumableItem
+{
+    [Header("Velocity")]
+    public int upwardVelocity = 50;
+    public int forwardVelocity = 50;
+    public int bombMass = 1;
+
+    [Header("Live Bomb Model")]
+    public GameObject liveBombModel;
+
+    [Header("Base Damage")]
+    public int baseDamage = 200;
+
+    public override void AttemptToConsumeItem(PlayerAnimatorManager animatorManager, PlayerWeaponSlotManager weaponSlotManager, PlayerEffectsManager playerEffectsManager)
+    {
+        if (currentItemAmount > 0)
+        {
+            animatorManager.PlayTargetAnimation(consumeAnimation, true);
+            GameObject bombModel = Instantiate(itemModel, weaponSlotManager.rightHandSlot.transform.position, Quaternion.identity, weaponSlotManager.rightHandSlot.transform);
+            playerEffectsManager.instantiatedFXModel = bombModel;
+        }
+        else
+        {
+            animatorManager.PlayTargetAnimation("shrug", true);
+        }
+    }
+}
---
+++ Assets/_Scripts/PoisonBuildUpBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoisonBuildUpBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.maxValue = 100;
+        slider.value = 0;
+        gameObject.SetActive(false);
+    }
+
+    public void SetPoisonBuildUpAmount(int currentPoisonBuildUp)
+    {
+        slider.value = currentPoisonBuildUp;
+    }
+}
