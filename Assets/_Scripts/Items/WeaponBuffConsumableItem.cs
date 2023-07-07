34999b51a4da76e60d037052cd473a4f1649a835
Jesper Lundquist <96360325+jeppaleno@users.noreply.github.com>
2023-07-07 19:02:12 +0200

    Character Effects - Fire Buff

--- Assets/_Scripts/Items/WeaponBuffConsumableItem.cs
+++ Assets/_Scripts/Items/WeaponBuffConsumableItem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Items/Consumeables/Weapon Buff")]
 public class WeaponBuffConsumableItem : ConsumableItem
 {
     [Header("Effect")]
