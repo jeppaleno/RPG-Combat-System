b708ff0d2172ca3489e2aa88e5ea04a830d18df9
Jesper Lundquist <96360325+jeppaleno@users.noreply.github.com>
2023-07-07 18:01:00 +0200

    Character Effects - Straight Sword SetUp

--- Assets/_Scripts/Equipment/MeleeWeaponItem.cs
+++ Assets/_Scripts/Equipment/MeleeWeaponItem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Items/Melee Weapon Item")]
 public class MeleeWeaponItem : WeaponItem
 {
     public bool canBeBuffed = true;
--- Assets/_Scripts/Items/Weapons/WeaponItem.cs
+++ Assets/_Scripts/Items/Weapons/WeaponItem.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Items/Weapon Item")]
 public class WeaponItem : Item
 {
     public GameObject modelPrefab;
